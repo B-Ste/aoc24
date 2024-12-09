a966c029978f0a1b95351587724c86159fef21e0
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-12-09 18:44:06 +0100

    solution for puzzle 2 part 1

---
+++ puzzle_02/puzzle_02.hs
@@ -0,0 +1,31 @@
+module Main where
+
+    main :: IO ()
+    main = do
+        input <- readFile "input.txt"
+        putStr (puzzle1 input)
+        putStr (puzzle2 input)
+
+    puzzle1 :: String -> String
+    puzzle1 input = show (calculate (parseInput input)) ++ "\n"
+        where
+            calculate :: [[Int]] -> Int
+            calculate x = length (filter safeReport x)
+
+            safeReport :: [Int] -> Bool
+            safeReport (x:xs) = let 
+                f   | x - head xs < 0 = (\x y -> y - x >= 1 && y - x <= 3) 
+                    | otherwise = (\x y -> x - y >= 1 && x - y <= 3) 
+                in safeReportTyped (x:xs) f
+
+            safeReportTyped :: [Int] -> (Int -> Int -> Bool) -> Bool
+            safeReportTyped [x] _ = True
+            safeReportTyped (x:xs) f
+                | f x (head xs) = safeReportTyped xs f
+                | otherwise = False
+
+    puzzle2 :: String -> String
+    puzzle2 input = "undefined\n"
+
+    parseInput :: String -> [[Int]]
+    parseInput input = map (map read . words) (lines input)
