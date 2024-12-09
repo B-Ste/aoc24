c714a98124b9d2660f42081e07195ce8e6fcf2a7
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-12-09 18:02:32 +0100

    solution for puzzle 1 part 2

--- puzzle_01/puzzle_01.hs
+++ puzzle_01/puzzle_01.hs
@@ -1,7 +1,7 @@
 module Main where
 
     import Data.List (sort)
-    
+
     main :: IO ()
     main = do
         input <- readFile "input.txt"
@@ -15,12 +15,23 @@ module Main where
             calculate ([], []) acc = acc
             calculate (x:xs, y:ys) acc = calculate (xs, ys) (acc + abs (x - y))
 
-            assembleLists :: String -> ([Int], [Int])
-            assembleLists input = assembleListsAcc (lines input) ([], [])
-
-            assembleListsAcc :: [String] -> ([Int], [Int]) -> ([Int], [Int])
-            assembleListsAcc [] (l1, l2)     = (sort l1, sort l2)
-            assembleListsAcc (x:xs) (l1, l2) = assembleListsAcc xs (read (head (words x)):l1, read (head (tail (words x))):l2)
-
     puzzle2 :: String -> String
-    puzzle2 _ = "undefined\n"
+    puzzle2 input = show (calculate (assembleLists input) 0) ++ "\n"
+        where
+            -- n^2 algorithm is tolerable for an input of only 1000 lines
+            calculate :: ([Int], [Int]) -> Int -> Int
+            calculate ([], _) acc = acc
+            calculate (x:xs, l2) acc = calculate (xs, l2) (acc + x * getTimes x l2 0)
+
+            getTimes :: Int -> [Int] -> Int -> Int
+            getTimes _ [] acc = acc
+            getTimes t (x:xs) acc 
+                | t == x = getTimes t xs (acc + 1)
+                | otherwise = getTimes t xs acc
+
+    assembleLists :: String -> ([Int], [Int])
+    assembleLists input = assembleListsAcc (lines input) ([], [])
+
+    assembleListsAcc :: [String] -> ([Int], [Int]) -> ([Int], [Int])
+    assembleListsAcc [] (l1, l2)     = (sort l1, sort l2)
+    assembleListsAcc (x:xs) (l1, l2) = assembleListsAcc xs (read (head (words x)):l1, read (head (tail (words x))):l2)
