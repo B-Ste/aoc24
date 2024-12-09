1fbf5f9958d3702fe55eb23eaf4fecb8c643e56d
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-12-09 19:11:27 +0100

    add solution for puzzle 2 part 2

--- puzzle_02/puzzle_02.hs
+++ puzzle_02/puzzle_02.hs
@@ -12,20 +12,30 @@ module Main where
             calculate :: [[Int]] -> Int
             calculate x = length (filter safeReport x)
 
-            safeReport :: [Int] -> Bool
-            safeReport (x:xs) = let 
-                f   | x - head xs < 0 = (\x y -> y - x >= 1 && y - x <= 3) 
-                    | otherwise = (\x y -> x - y >= 1 && x - y <= 3) 
-                in safeReportTyped (x:xs) f
-
-            safeReportTyped :: [Int] -> (Int -> Int -> Bool) -> Bool
-            safeReportTyped [x] _ = True
-            safeReportTyped (x:xs) f
-                | f x (head xs) = safeReportTyped xs f
-                | otherwise = False
-
     puzzle2 :: String -> String
-    puzzle2 input = "undefined\n"
+    puzzle2 input = show (calculate (parseInput input)) ++ "\n"
+        where
+            calculate :: [[Int]] -> Int
+            calculate x = length (filter safeReportDampened x)
 
     parseInput :: String -> [[Int]]
     parseInput input = map (map read . words) (lines input)
+
+    safeReport :: [Int] -> Bool
+    safeReport (x:xs) = let
+        f   | x - head xs < 0 = (\x y -> y - x >= 1 && y - x <= 3)
+            | otherwise = (\x y -> x - y >= 1 && x - y <= 3)
+        in safeReportTyped (x:xs) f
+
+    safeReportTyped :: [Int] -> (Int -> Int -> Bool) -> Bool
+    safeReportTyped [x] _ = True
+    safeReportTyped (x:xs) f
+        | f x (head xs) = safeReportTyped xs f
+        | otherwise = False
+
+    safeReportDampened :: [Int] -> Bool
+    safeReportDampened = safeReportDampenedAcc []
+
+    safeReportDampenedAcc :: [Int] -> [Int] -> Bool
+    safeReportDampenedAcc ys [] = safeReport ys
+    safeReportDampenedAcc ys (x:xs) = safeReport (ys ++ xs) || safeReportDampenedAcc (ys ++ [x]) xs
