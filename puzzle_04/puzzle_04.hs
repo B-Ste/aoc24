5b0f8fa4bfae3a3011b0698df9fb484cb5b85171
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-12-10 21:07:55 +0100

    solution for puzzle 4 part 2

--- puzzle_04/puzzle_04.hs
+++ puzzle_04/puzzle_04.hs
@@ -16,9 +16,6 @@ module Main where
         + parseXMASStringSet (diagonalise (unlines (reverse (lines input))))
         + parseXMASStringSet (diagonalise (transpose (unlines (reverse (lines input)))))
 
-    puzzle2 :: String -> String
-    puzzle2 input = "undefined\n"
-
     parseXMASStringSet :: String -> Int
     parseXMASStringSet x = parseXMASString x 0
 
@@ -41,11 +38,23 @@ module Main where
         where
             diagonaliseLines :: [String] -> [String] -> [String] -> [String]
             diagonaliseLines [] [] acc = acc
-            diagonaliseLines xs [] acc = diagonaliseLines (removeFirstElements xs) [] (acc ++ [firstElementsToString xs])
-            diagonaliseLines xs (y:ys) acc = diagonaliseLines (removeFirstElements (xs ++ [y])) ys (acc ++ [firstElementsToString (xs ++ [y])])
-
-    firstElementsToString :: [String] -> String
-    firstElementsToString = map head
+            diagonaliseLines xs [] acc = diagonaliseLines (removeFirstElements xs) [] (acc ++ [map head xs])
+            diagonaliseLines xs (y:ys) acc = diagonaliseLines (removeFirstElements (xs ++ [y])) ys (acc ++ [map head (xs ++ [y])])
 
     removeFirstElements :: [String] -> [String]
     removeFirstElements s = filter (/= "") (map tail s)
+
+    puzzle2 :: String -> Int
+    puzzle2 input = parseX_MASString (lines input) 0
+
+    parseX_MASString :: [String] -> Int -> Int
+    parseX_MASString (a:b:c:xs) i = parseX_MASString (b:c:xs) (i + masCount a b c 0)
+    parseX_MASString _ i = i
+
+    masCount :: String -> String -> String -> Int -> Int
+    masCount [] [] [] i = i
+    masCount ('M':a:'M':xs) (b:'A':c:ys) ('S':d:'S':zs) i = masCount (a:'M':xs) ('A':c:ys) (d:'S':zs) (i + 1)
+    masCount ('S':a:'M':xs) (b:'A':c:ys) ('S':d:'M':zs) i = masCount (a:'M':xs) ('A':c:ys) (d:'M':zs) (i + 1)
+    masCount ('M':a:'S':xs) (b:'A':c:ys) ('M':d:'S':zs) i = masCount (a:'S':xs) ('A':c:ys) (d:'S':zs) (i + 1)
+    masCount ('S':a:'S':xs) (b:'A':c:ys) ('M':d:'M':zs) i = masCount (a:'S':xs) ('A':c:ys) (d:'M':zs) (i + 1)
+    masCount (a:xs) (b:ys) (c:zs) i = masCount xs ys zs i
