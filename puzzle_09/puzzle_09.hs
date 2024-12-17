2272e269bebe9a45f9e3059f3be8978591955bd5
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-12-17 23:54:16 +0100

    solution for puzzle 9 part 1

---
+++ puzzle_09/puzzle_09.hs
@@ -0,0 +1,46 @@
+module Main where
+    import Debug.Trace (trace)
+
+    main :: IO ()
+    main = do
+        input <- readFile "input.txt"
+        (print . puzzle1) input
+        (print . puzzle2) input
+
+    parseInput :: String -> [Maybe Int]
+    parseInput = parseInputAcc 0
+        where
+            parseInputAcc :: Int -> String -> [Maybe Int]
+            parseInputAcc id [x] = replicate (read [x]) (Just id)
+            parseInputAcc id (x:y:zs) =
+                replicate (read [x]) (Just id)
+                ++ replicate (read [y]) Nothing
+                ++ parseInputAcc (id + 1) zs
+
+    compress :: [Maybe Int] -> [Int]
+    compress s = take (length s - emptySpace s) (compressAcc s (reverse s))
+        where
+            compressAcc :: [Maybe Int] -> [Maybe Int] -> [Int]
+            compressAcc [] _ = []
+            compressAcc _ [] = []
+            compressAcc a (Nothing:bs) = compressAcc a bs
+            compressAcc (Just a:as) b = a : compressAcc as b
+            compressAcc (Nothing:as) (Just b:bs) = b : compressAcc as bs
+
+            emptySpace :: [Maybe Int] -> Int
+            emptySpace [] = 0
+            emptySpace (Nothing:xs) = 1 + emptySpace xs
+            emptySpace (x:xs) = emptySpace xs
+
+    calculateChecksum :: [Int] -> Int
+    calculateChecksum = calculateChecksumAcc 0
+        where
+            calculateChecksumAcc :: Int -> [Int] -> Int
+            calculateChecksumAcc _ [] = 0
+            calculateChecksumAcc pos (x:xs) = pos * x + calculateChecksumAcc (pos + 1) xs
+
+    puzzle1 :: String -> Int
+    puzzle1 = calculateChecksum . compress . parseInput
+
+    puzzle2 :: String -> Int
+    puzzle2 = undefined
