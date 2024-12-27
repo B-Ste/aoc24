466a7275f4e05965a2ae6ef70936eea27a0c01f8
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-12-27 18:14:47 +0100

    solution for puzzle 18 part 2

--- puzzle_18/puzzle_18.hs
+++ puzzle_18/puzzle_18.hs
@@ -8,13 +8,13 @@ module Main where
     main = do
         input <- readFile "input.txt"
         print . puzzle1 $ input
+        print . puzzle2 $ input
 
     parseInput :: Int -> String -> Set Position
     parseInput i = Set.fromList . take i . map parseCoordinate . lines
         where
             parseCoordinate :: String -> Position
-            parseCoordinate s = let (f, k) = break (== ',') s in
-                (read f, read . tail $ k)
+            parseCoordinate s = let (f, k) = break (== ',') s in (read f, read . tail $ k)
 
     bfs :: Position -> Set Position -> [(Position, Int)]
     bfs p bar = bfsAcc [(p, 0)] Set.empty
@@ -23,8 +23,7 @@ module Main where
             bfsAcc [] _  = []
             bfsAcc (x : xs) seen = let 
                 ((py, px), d) = x
-                n = [(pn, d + 1) 
-                    | pn <- [(py + 1, px), (py - 1, px), (py, px + 1), (py, px - 1)], 
+                n = [(pn, d + 1) | pn <- [(py + 1, px), (py - 1, px), (py, px + 1), (py, px - 1)], 
                     inBounds pn && pn `Set.notMember` bar && pn `Set.notMember` seen]
                 in x : bfsAcc (xs ++ n) (Set.union seen $ Set.fromList . map fst $ n)
                 where
@@ -33,4 +32,16 @@ module Main where
 
     puzzle1 :: String -> Int
     puzzle1 = snd . head . dropWhile (\(p, _) -> p /= (70, 70)) . bfs (0, 0) . parseInput 1024
-    
+
+    puzzle2 :: String -> String
+    puzzle2 s = let i = binSearch 3449 1024 in lines s !! i
+        where
+            binSearch :: Int -> Int -> Int
+            binSearch top btm = 
+                if top == btm then top
+                else let mid = (top + btm) `div` 2 in
+                    if not . null . exitSearch s $ mid then binSearch top (mid + 1)
+                    else binSearch (mid - 1) btm
+ 
+            exitSearch :: String -> Int -> [(Position, Int)]
+            exitSearch s i = dropWhile (\(p, _) -> p /= (70, 70)) . bfs (0, 0) . parseInput i $ s
