b391f60cd49f0121a237c3b17435a7804a430e96
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-12-29 10:42:15 +0100

    small refactor

--- puzzle_17/puzzle_17.hs
+++ puzzle_17/puzzle_17.hs
@@ -1,6 +1,7 @@
 module Main where
-    import Data.Bits (xor)
-    import Control.Monad (void)
+    import Data.Bits (xor, shiftR, shiftL, (.|.), complement)
+    import Data.List (nub)
+    import Debug.Trace (traceShowId, trace)
 
     -- A B C PC Prog
     data Computer = C Int Int Int Int [Int]
@@ -14,7 +15,7 @@ module Main where
     main :: IO ()
     main = do
         input <- readFile "input.txt"
-        puzzle1 input
+        print . puzzle1 $ input
 
     parseInput :: String -> Computer
     parseInput s = let
@@ -24,22 +25,20 @@ module Main where
         c = read . last . words $ ls !! 2
         in C a b c 0 $ tail . map read . wordsWhen (\x -> x == ' ' || x == ',') $ ls !! 4
 
-    evaluate :: IO Computer -> IO Computer
-    evaluate mc = do
-        comp <- mc
-        let (C a b c pc p) = comp in
-            if pc >= length p then return (C a b c pc p)
-            else case p !! pc of
-                0 -> evaluate $ return $ C (truncate $ toRational a / (2 ^ comb comp (op p pc))) b c (pc + 2) p
-                1 -> evaluate $ return $ C a (b `xor` op p pc) c (pc + 2) p
-                2 -> evaluate $ return $ C a (comb comp (op p pc) `mod` 8) c (pc + 2) p
-                3 -> if a /= 0
-                    then evaluate $ return $ C a b c (op p pc) p
-                    else evaluate $ return $ C a b c (pc + 2) p
-                4 -> evaluate $ return $ C a (b `xor` c) c (pc + 2) p
-                5 -> putStr (show (comb comp (op p pc) `mod` 8) ++ [',']) >> evaluate (return $ C a b c (pc + 2) p)
-                6 -> evaluate $ return $ C a (truncate $ toRational a / (2 ^ comb comp (op p pc))) c (pc + 2) p
-                7 -> evaluate $ return $ C a b (truncate $ toRational a / (2 ^ comb comp (op p pc))) (pc + 2) p
+    evaluate :: Computer -> [Int]
+    evaluate comp =let (C a b c pc p) = comp in
+        if pc >= length p then []
+        else case p !! pc of
+            0 -> evaluate $ C (truncate $ toRational a / (2 ^ comb comp (op p pc))) b c (pc + 2) p
+            1 -> evaluate $ C a (b `xor` op p pc) c (pc + 2) p
+            2 -> evaluate $ C a (comb comp (op p pc) `mod` 8) c (pc + 2) p
+            3 -> if a /= 0
+                then evaluate $ C a b c (op p pc) p
+                else evaluate $ C a b c (pc + 2) p
+            4 -> evaluate $ C a (b `xor` c) c (pc + 2) p
+            5 -> (comb comp (op p pc) `mod` 8) : evaluate (C a b c (pc + 2) p)
+            6 -> evaluate $ C a (truncate $ toRational a / (2 ^ comb comp (op p pc))) c (pc + 2) p
+            7 -> evaluate $ C a b (truncate $ toRational a / (2 ^ comb comp (op p pc))) (pc + 2) p
         where
             op :: [Int] -> Int -> Int
             op l i = l !! (i + 1)
@@ -49,9 +48,9 @@ module Main where
             comb _ 1 = 1
             comb _ 2 = 2
             comb _ 3 = 3
-            comb (C a b c pc p) 4 = a
-            comb (C a b c pc p) 5 = b
-            comb (C a b c pc p) 6 = c
+            comb (C a _ _ _ _) 4 = a
+            comb (C _ b _ _ _) 5 = b
+            comb (C _ _ c _ _) 6 = c
 
-    puzzle1 :: String -> IO ()
-    puzzle1 s = Control.Monad.void (evaluate (return $ parseInput s) >> putChar '\n')
+    puzzle1 :: String -> [Int]
+    puzzle1 = evaluate . parseInput
