98b96b3260548c62c1dc8bcb4d63aeda8e43effa
uykfh@student.kit.edu <benni.steeg@icloud.com>
2025-01-29 15:58:49 +0100

    refactor

--- puzzle_01/puzzle_01.hs
+++ puzzle_01/puzzle_01.hs
@@ -5,33 +5,19 @@ module Main where
     main :: IO ()
     main = do
         input <- readFile "input.txt"
-        putStr (puzzle1 input)
-        putStr (puzzle2 input)
+        print . puzzle1 $ input
+        print . puzzle2 $ input
 
-    puzzle1 :: String -> String
-    puzzle1 input = show (calculate (assembleLists input) 0) ++ "\n"
+    puzzle1 :: String -> Int
+    puzzle1 input = sum . zipWith (\a b -> abs (a - b)) (list1 input) $ list2 input
+
+    puzzle2 :: String -> Int
+    puzzle2 input = sum . map (\x -> x * length (filter (==x) l2)) $ list1 input
         where
-            calculate :: ([Int], [Int]) -> Int -> Int
-            calculate ([], []) acc = acc
-            calculate (x:xs, y:ys) acc = calculate (xs, ys) (acc + abs (x - y))
+            l2 = list2 input
 
-    puzzle2 :: String -> String
-    puzzle2 input = show (calculate (assembleLists input) 0) ++ "\n"
-        where
-            -- n^2 algorithm is tolerable for an input of only 1000 lines
-            calculate :: ([Int], [Int]) -> Int -> Int
-            calculate ([], _) acc = acc
-            calculate (x:xs, l2) acc = calculate (xs, l2) (acc + x * getTimes x l2 0)
+    list1 :: String -> [Int]
+    list1 = sort . map (read . head . words) . lines
 
-            getTimes :: Int -> [Int] -> Int -> Int
-            getTimes _ [] acc = acc
-            getTimes t (x:xs) acc 
-                | t == x = getTimes t xs (acc + 1)
-                | otherwise = getTimes t xs acc
-
-    assembleLists :: String -> ([Int], [Int])
-    assembleLists input = assembleListsAcc (lines input) ([], [])
-
-    assembleListsAcc :: [String] -> ([Int], [Int]) -> ([Int], [Int])
-    assembleListsAcc [] (l1, l2)     = (sort l1, sort l2)
-    assembleListsAcc (x:xs) (l1, l2) = assembleListsAcc xs (read (head (words x)):l1, read (head (tail (words x))):l2)
+    list2 :: String -> [Int]
+    list2 = sort . map (read . head . tail . words) . lines
